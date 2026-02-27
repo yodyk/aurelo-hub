d67d733cf48676015bbc479d728099bed9fb655d
yodyk <152555388+yodyk@users.noreply.github.com>
2026-02-26 23:32:31 -0500

    Update import path for react-router in routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router";
 import Root from "./pages/Root";
 import Home from "./pages/Home";
 import Clients from "./pages/Clients";
